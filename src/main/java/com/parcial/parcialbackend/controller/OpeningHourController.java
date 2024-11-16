57568083fa9fcc003b1590e08748ed582057991c
Robert Navarro <robertlorenzonavarrovalle2@gmail.com>
2024-11-16 13:26:23 -0400

    actualizado despues del pull

--- src/main/java/com/parcial/parcialbackend/controller/OpeningHourController.java
+++ src/main/java/com/parcial/parcialbackend/controller/OpeningHourController.java
@@ -3,7 +3,6 @@ package com.parcial.parcialbackend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +32,5 @@ public class OpeningHourController {
         return ResponseEntity.ok(openingHourService.getAllOpeningHours());
     }
 
-    
+
 }
